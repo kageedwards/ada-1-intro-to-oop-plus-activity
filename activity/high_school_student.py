2b99e18b73a81dd5531e1743df969bff230404a7
Kage Edwards <kage.edwards1992@gmail.com>
2026-09-22 11:52:29 -0700

    Refactor summary for HighSchoolStudent
    Co-authored-by: Najma Shanle <najmashanle22@gmail.com>

--- activity/high_school_student.py
+++ activity/high_school_student.py
@@ -1,10 +1,11 @@
 from .student import Student
 
 class HighSchoolStudent(Student):
-    def __init__(self, name, grade, classes, has_parking_privileges=False, clubs=[]):
-        super().__init__(self, name, grade, classes)
+
+    def __init__(self, name, grade, classes, has_parking_privileges=False, clubs=None):
+        super().__init__(name, grade, classes)
         self.has_parking_privileges = has_parking_privileges
-        self.clubs = clubs
+        self.clubs = [] if clubs is None else clubs
 
     def join_club(self, club):
         if club not in self.clubs:
@@ -19,9 +20,14 @@ class HighSchoolStudent(Student):
         return "has parking privileges" if self.has_parking_privileges else "doesn't have parking privileges"
 
     def display_clubs(self):
-        return ", ".join(self.clubs)
+            if not self.clubs:
+                return ""
+    
+            clubs = ", ".join(self.clubs)
+            return f", is in {self.get_num_clubs()} clubs (including {clubs})"
 
     def summary(self):
-        gen_summary = super().summary()
+        gen_summary = f"{self.name} is a {self.grade} enrolled in {self.get_num_classes()} classes"
+        club_summary = self.display_clubs()
         parking_perm = self.display_parking_privileges()
-        return gen_summary + f" , is in {self.get_num_clubs()} (including {self.display_clubs()}), and {parking_perm}"
+        return gen_summary + club_summary + f" and {parking_perm}"
